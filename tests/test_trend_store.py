6ad2ed202ef76ff42161f6788f4ed96e6f8d7cf8
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-08-12 22:09:50 +0700

    pass test trend store

---
+++ tests/test_trend_store.py
@@ -0,0 +1,39 @@
+from app.storage.trend_store import (
+    save_snapshot,
+    load_snapshots,
+)
+
+
+def test_save_snapshot():
+
+    videos = [
+        {
+            "video_id": "test123",
+            "views": 100000,
+            "likes": 5000,
+            "comments": 1000,
+        }
+    ]
+
+    file = save_snapshot(
+        videos
+    )
+
+    assert file.exists()
+
+
+def test_load_snapshots():
+
+    snapshots = load_snapshots()
+
+    assert len(snapshots) > 0
+
+    assert (
+        "timestamp"
+        in snapshots[-1]
+    )
+
+    assert (
+        "videos"
+        in snapshots[-1]
+    )
