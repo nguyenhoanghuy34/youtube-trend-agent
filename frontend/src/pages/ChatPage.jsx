8b358d80af23720b14c47bb25c9cdb7e96324d9e
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-08-17 16:07:18 +0700

    fix report panel

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,20 @@
 import ChatPanel from "../components/chat/ChatPanel";
+import ReportPanel from "../components/reports/ReportPanel";
 
 export default function ChatPage() {
   return (
-    <div className="h-full">
-      <ChatPanel />
+    <div className="grid h-full min-h-0 grid-cols-2">
+
+      {/* Left - Chat */}
+      <div className="min-h-0 border-r border-slate-200 bg-white">
+        <ChatPanel />
+      </div>
+
+      {/* Right - Reports */}
+      <div className="min-h-0 bg-slate-50">
+        <ReportPanel />
+      </div>
+
     </div>
   );
 }
