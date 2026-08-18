c726734e6ba179f7ee4a323bd3eb2bcebb3dfda0
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-08-18 16:56:09 +0700

    fix bug no Video Card

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 import ChatPanel from "../components/chat/ChatPanel";
 import ReportPanel from "../components/reports/ReportPanel";
 
 export default function ChatPage() {
+  const [videos, setVideos] = useState([]);
+
   return (
     <div className="grid h-full min-h-0 grid-cols-2">
 
       {/* Left - Chat */}
       <div className="min-h-0 border-r border-slate-200 bg-white">
-        <ChatPanel />
+        <ChatPanel
+          onVideosUpdate={setVideos}
+        />
       </div>
 
       {/* Right - Reports */}
       <div className="min-h-0 bg-slate-50">
-        <ReportPanel />
+        <ReportPanel
+          videos={videos}
+        />
       </div>
 
     </div>
